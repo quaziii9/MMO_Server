f51570d557132893a06998a0c87a9fd2699e3834
Eunjoo <quaziii9gamedev@gmail.com>
2024-10-08 12:06:53 +0900

    [feat] ObjectManager

--- Client/Assets/Scripts/Controllers/CreatureController.cs
+++ Client/Assets/Scripts/Controllers/CreatureController.cs
@@ -6,8 +6,7 @@ using static Define;
 public class CreatureController : MonoBehaviour
 {
     public float _speed = 5.0f;
-
-    protected Vector3Int _cellPos = Vector3Int.zero;
+    public Vector3Int CellPos { get; set; } = Vector3Int.zero;
     protected Animator _animator;
     protected SpriteRenderer _sprite;
     CreatureState _state = CreatureState.Idle;
@@ -115,7 +114,7 @@ public class CreatureController : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _sprite = GetComponent<SpriteRenderer>();
-        Vector3 pos = Managers.Map.CurrentGrid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
+        Vector3 pos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f);
         transform.position = pos;
     }
 
@@ -131,7 +130,7 @@ public class CreatureController : MonoBehaviour
         if (State != CreatureState.Moving)
             return;
 
-        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.5f);
+        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f);
         Vector3 moveDir = destPos - transform.position;
 
         // 도착 여부 체크
@@ -156,7 +155,7 @@ public class CreatureController : MonoBehaviour
     {
         if (State == CreatureState.Idle && _dir != MoveDir.None)
         {
-            Vector3Int destPos = _cellPos;
+            Vector3Int destPos = CellPos;
             switch (_dir)
             {
                 case MoveDir.Up:
@@ -172,11 +171,14 @@ public class CreatureController : MonoBehaviour
                     destPos += Vector3Int.right;
                     break;
             }
+            State = CreatureState.Moving;
 
             if (Managers.Map.CanGo(destPos))
             {
-                _cellPos = destPos;
-                State = CreatureState.Moving;
+                if (Managers.Object.Find(destPos) == null)
+                {
+                    CellPos = destPos;
+                }
             }
         }
     }
